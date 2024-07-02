11555ee14c861d34aacc04496d32b33758b76f7a
shubham Raundal <manaslahare@gmail.com>
2024-07-02 23:22:14 +0530

    manas

---
+++ includes/fleftbar.php
@@ -0,0 +1,59 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">Faculty</h6>
+                                <small class="info">Staff</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="fdashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Course</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="fcreate-course.php"><i class="fa fa-bars"></i> <span>Create Course</span></a></li>
+                                            <li><a href="fmanage-course.php"><i class="fa fa fa-server"></i> <span>Manage Course</span></a></li>
+                                           <li><a href="fadd-coursecombination.php"><i class="fa fa-newspaper-o"></i> <span>Add Course Combination </span></a></li>
+                                           <a href="fmanage-coursecombination.php"><i class="fa fa-newspaper-o"></i> <span>Manage Course Combination </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="fadd-students.php"><i class="fa fa-bars"></i> <span>Add Students</span></a></li>
+                                            <li><a href="fmanage-students.php"><i class="fa fa fa-server"></i> <span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="fadd-result.php"><i class="fa fa-bars"></i> <span>Add Result</span></a></li>
+                                            <li><a href="fmanage-results.php"><i class="fa fa fa-server"></i> <span>Manage Result</span></a></li>
+                                           
+                                        </ul>        </li>
+
+
+
+
+
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> Faculty Change Password</span></a></li>
+                                           
+                            
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
